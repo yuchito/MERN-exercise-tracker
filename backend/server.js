3180de2ce75661a5a21c2abe35d2c74ee521eb1c
HoussamBenhadine <houssambenhadine21@gmail.com>
2019-10-14 12:22:40 +0200

    Loading the routers and add them as middlewares

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
 
 app.use(cors());
 app.use(express.json());
+
+
+
 const uri = process.env.MONGO_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreatedIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('MongoDB db connection established successfully');
 });
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter)
 
 app.listen(port, () => {
-    console.log('Server running on port: ${port}')
+    console.log(`Server running on port: ${port}`);
 })
