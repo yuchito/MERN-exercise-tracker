4efe6a0d48ecc823556e32ad5e2fd8eb772bc0f6
HoussamBenhadine <houssambenhadine21@gmail.com>
2019-11-02 23:08:18 +0000

    edit exercise

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -1,10 +1,155 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 export default class EditExercise extends Component {
+    //Add a constructor to initialize the state props and to bind this to methods
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: '',
+      description: '',
+      duration: 0,
+      date: new Date(),
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    //Send an HTTP GET request to the backend endpoint
+    //Get the exercise to edit
+    axios.get('http://localhost:5000/exercises/'+this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          username: response.data.username,
+          description: response.data.description,
+          duration: response.data.duration,
+          date: new Date(response.data.date)
+        })   
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+
+    // Send an HTTP GET request to the backend endpoint
+    // Get the user list from the DB and add it to dropmenu in the form
+    axios.get('http://localhost:5000/users/')
+      .then(response => {
+        this.setState({ users: response.data.map(user => user.username) });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+  
+  // methods to update the props of state
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    });
+  }
+    
+  // handling date using date picker library
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+    
+  // handle submit event of the form
+  onSubmit(e) {
+
+    // prevent the default HTML form submit behavior
+    e.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date,
+    };
+
+    // console.log(exercise);
+
+    // update the edired exercise and as second arg,cuz endpoint is expecting a JSON object in the req body
+    axios.post('http://localhost:5000/exercises/update/'+this.props.match.params.id, exercise)
+      .then(res => console.log(res.data));
+           
+    // after submitting the user is taken back to the home page
+    window.location = '/';
+  }
+
   render() {
     return (
       <div>
-        <p>You are on the Edit Exercise component!</p>
+        <h3>Edit Exercise Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <select ref="userInput"
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}>
+                {
+                  this.state.users.map(function(user) {
+                    return <option 
+                      key={user}
+                      value={user}>{user}
+                      </option>;
+                  })
+                }
+            </select>
+          </div>
+          <div className="form-group"> 
+            <label>Description: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={this.state.description}
+                onChange={this.onChangeDescription}
+                />
+          </div>
+          <div className="form-group">
+            <label>Duration (in minutes): </label>
+            <input 
+                type="text" 
+                className="form-control"
+                value={this.state.duration}
+                onChange={this.onChangeDuration}
+                />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <DatePicker
+              selected={this.state.date}
+              onChange={this.onChangeDate}
+            />
+          </div>
+
+          <div className="form-group">
+            <input type="submit" value="Edit Exercise Log" className="btn btn-success" />
+          </div>
+        </form>
       </div>
     )
   }
